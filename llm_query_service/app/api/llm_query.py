6fbebdc35ff484efff11d64958f8fdc4ab4a7105
chuckbasstan123 <xt307@nyu.edu>
2023-04-07 23:01:51 -0700

    add gcp deployment

--- llm_query_service/app/api/llm_query.py
+++ llm_query_service/app/api/llm_query.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, request, jsonify
 from app.services.chatgpt_service import ChatGPTService
-from config import OPENAI_MODEL, OPENAI_API_KEY
+from app.config import OPENAI_MODEL, OPENAI_API_KEY
 
 print(OPENAI_API_KEY, OPENAI_MODEL)
 chatgpt_blueprint = Blueprint('chatgpt', __name__)
 chatgpt_service = ChatGPTService(api_key=OPENAI_API_KEY, engine=OPENAI_MODEL)
+
 
 @chatgpt_blueprint.route('/chatgpt/query', methods=['POST'])
 def chatgpt_query():
