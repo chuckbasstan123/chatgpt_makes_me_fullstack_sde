a5512f9270988c7b9bccf76cc59b4b9251a75978
chuckbasstan123 <xt307@nyu.edu>
2023-04-13 21:21:27 -0700

    update ui code

--- llm_ui/src/APP.js
+++ llm_ui/src/APP.js
@@ -6,12 +6,33 @@ import UserInput from './components/UserInput';
 const App = () => {
   const [messages, setMessages] = useState([]);
 
-  const handleUserMessage = (text) => {
-    setMessages([
-      ...messages,
-      { sender: 'user', text: 'hello, I am your personal life coach.' },
-    ]);
-    // Implement chatbot logic or API call here, and update messages with the chatbot's response
+  const sendMessage = async (message) => {
+    setMessages([...messages, message]);
+  };
+
+  const handleUserMessage = async (text) => {
+    // Add the user's message to the state
+    await sendMessage({ text: 'none sense', sender: 'user' });
+
+    // Uncomment and update the API call to the ChatGPT API
+    const postData = {
+      prompt: text,
+    };
+
+    // try {
+    const response = await fetch('http://34.75.228.61:5000/chatgpt/query', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer' + 'AIzaSyCIPbT0szCLc5kX_TmC3zjKWoptK8WkSZY',
+      },
+      body: JSON.stringify(postData),
+    });
+    const data = await response.json();
+    await sendMessage({ text: data.response, sender: 'chatbot' });
+    // } catch (error) {
+    //   console.error(error);
+    // }
   };
 
   return (
@@ -19,6 +40,7 @@ const App = () => {
       <header className="App-header">
         <h1>Simple Chatbot UI LLM </h1>
       </header>
+
       <ChatBox messages={messages} />
       <UserInput onSubmit={handleUserMessage} />
     </div>
@@ -26,19 +48,3 @@ const App = () => {
 };
 
 export default App;
-
-// import React from 'react';
-// import './App.css';
-// // import Chatbot from './components/Chatbot';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Simple Chatbot UI</h1>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
